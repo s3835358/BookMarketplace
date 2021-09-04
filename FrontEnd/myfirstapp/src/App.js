f65865c93ba45e3c03c8767b76cb1a92612f576e
s3835358 <s3835358@student.rmit.edu.au>
2021-09-04 10:07:38 +1000

    Update JWT infrastructure

--- FrontEnd/myfirstapp/src/App.js
+++ FrontEnd/myfirstapp/src/App.js
@@ -13,6 +13,22 @@ import Register from "./components/UserManagement/Register";
 import Login from "./components/UserManagement/Login";
 import Catalogue from "./components/Catalogue";
 
+import jwt_decode from "jwt-decode";
+import setJWTToken from "./securityUtils/setJWTToken";
+import { SET_CURRENT_USER } from "./actions/types";
+import { logout } from "./actions/securityActions";
+import SecuredRoute from "./securityUtils/SecureRoute";
+
+const jwtToken = localStorage.jwtToken;
+
+if (jwtToken) {
+  setJWTToken(jwtToken);
+  const decoded_jwtToken = jwt_decode(jwtToken);
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: decoded_jwtToken
+  });
+
 class App extends Component {
   render() {
     return (
