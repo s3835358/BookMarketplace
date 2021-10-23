f176ce5f8082c593b6b39ae56d1e5c834d10fecd
s3835358 <s3835358@student.rmit.edu.au>
2021-10-23 14:12:17 +1100

    Added transaction history

--- FrontEnd/myfirstapp/src/components/UserManagement/TransactionHistory.js
+++ FrontEnd/myfirstapp/src/components/UserManagement/TransactionHistory.js
@@ -1,20 +1,71 @@
-import React, {useState, useMemo} from "react";
-
+import React, {useState, useMemo, useEffect} from "react";
+import axios from 'axios';
+import store from "../../store";
+import {useUserContext} from '../UserManagement/UserContext';
 import Table from '../Layout/table';
 import '../Background.css';
 
 export const TransactionHistory = props => {
     
-    const [orders] = useState([]);
+    const [history, setHistory] = useState([]);
+    const [logged, setLogged] = useState(false);
+    const {user} = useUserContext();
 
-    const orderCols = useMemo(
+    useEffect (() => {
+      if((user === "") && (localStorage.getItem("user") === null)) {
+          setLogged(false);
+      } else {
+          setLogged(true);
+      }
+
+      var req = {}
+      
+      if(logged){
+        req = {
+          "id": store.getState().security.user.id
+        }
+      }
+      
+
+      if(store.getState().security.user.userType.match("admin")) {
+        
+        axios.post("https://sept-orders.herokuapp.com/orders/getAll", req).then((response) => {         
+
+          console.log(response);
+
+          if(response.data.length > 0) {
+            setHistory(response.data);
+          }
+      
+        });
+
+      } else if (logged) {
+        axios.post("https://sept-orders.herokuapp.com/orders/getOrders", req).then((response) => {         
+
+          console.log(response);
+
+          if(response.data.length > 0) {
+            setHistory(response.data);
+          }
+      
+        });
+      }
+      
+
+    }, [user]);
+
+
+  
+    const historyCols = useMemo(
       () => [
         {Header: 'Order ID', accessor: 'id',},
-        {Header: 'Seller', accessor: 'sellerId',},
-        {Header: 'Buyer', accessor: 'buyerId',},
+        {Header: 'Seller ID', accessor: 'seller_id',},
+        {Header: 'Buyer ID', accessor: 'buyer_id',},
         {Header: 'Qty', accessor: 'qty',},
         {Header: 'Price', accessor: 'price',},
-        {Header: 'Title', accessor: 'bookTitle',},
+        {Header: 'Book ID', accessor: 'book_id',},
+        {Header: 'Title', accessor: 'book_title',},
+        {Header: 'Date', accessor: 'ordered_at',},
       ],
       []
     ); 
@@ -22,11 +73,9 @@ export const TransactionHistory = props => {
     return (
         <div className="background4" style ={{alignItems:"center", display:"flex", flexDirection:"column", justifyContent:"center"}}>
                     
-          
-                
             <div style ={{alignItems:"center", display:"flex", 
             flexDirection:"column", justifyContent:"center"}}>
-                <Table columns={orderCols} data={orders} />
+                <Table columns={historyCols} data={history} />
                 <div type = "button" className="btn btn-info btn-block mt-4"> Download</div>
                 <p style={{color:"white"}}>Transactions as CSV</p>
             </div>
